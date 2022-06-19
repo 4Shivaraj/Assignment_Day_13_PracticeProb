4792c2caae80c169ae401aeecccaafbaaa3e9358
4Shivaraj <4shivaraj.gowda@gmail.com>
2022-06-19 21:47:38 +0530

    [Shivaraj] Implemented Maximum of Three Floating Number

--- GenericsProblems/GenericsProblems/MaxUsingGenerics.cs
+++ GenericsProblems/GenericsProblems/MaxUsingGenerics.cs
@@ -24,7 +24,27 @@ namespace GenericsProblems
             }
                  
         }
-        
+         //UC 2: Given 3 Floats Find The Maximum.
+        public double GetMaximumFloat(double first, double second, double third)
+        {
+            if (first.CompareTo(second) > 0 && first.CompareTo(third) > 0)
+            {
+                return first;
+            }
+            else if (second.CompareTo(first) > 0 && second.CompareTo(third) > 0)
+            {
+                return second;
+            }
+            else if (third.CompareTo(first) > 0 && third.CompareTo(second) > 0)
+            {
+                return third;
+            }
+            else
+            {
+                Console.WriteLine("\teither 2 or 3 having same value/equals");
+                return 0.0;
+            }
+        }
     }
-    
 }
+ 
