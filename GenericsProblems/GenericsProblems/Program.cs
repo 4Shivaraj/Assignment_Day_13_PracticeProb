056561e1a3aed3bb089857dde5901987289140ca
4Shivaraj <4shivaraj.gowda@gmail.com>
2022-06-19 21:21:04 +0530

    [Shivaraj] Determined Max of Intergers

--- GenericsProblems/GenericsProblems/Program.cs
+++ GenericsProblems/GenericsProblems/Program.cs
@@ -1,2 +1,59 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Welcome to generics problems");
+//Console.WriteLine("Welcome to generics problems");
+namespace GenericsProblems
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Please Choose The Option Can Be Executed: ");
+            Console.WriteLine("1. Find the Max Value Out Of Three: \n2. Exit: ");
+            Console.Write("Enter the selected Option: ");
+            int option = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine("Enter the First Number: ");
+            int first_Number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the Second Number: ");
+            int second_Number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the Third Number: ");
+            int third_Number = Convert.ToInt32(Console.ReadLine());
+
+            switch (option)
+            {
+                case 1:
+                    
+                    FindMaxNum findMaxNum = new FindMaxNum();
+                    if (findMaxNum.GetMaximumInteger(first_Number, second_Number, third_Number) != 0)
+                        Console.WriteLine("{0} is Maximum out of {1} ,{2}, {3}", findMaxNum.GetMaximumInteger(first_Number, second_Number, third_Number), first_Number, second_Number, third_Number);
+                    break;
+                case 2:
+                default:
+                    break;
+            }
+            Console.ReadLine();
+            
+        }
+    }
+    
+}
+
+//UC-1
+//Given 3 Integers find the maximum
+//- Ensure to test code with the Test Case.
+//- To ensure your Code works you need 3 test cases with Max Number at 1st, 2nd and 3rd
+//- Use Integer Object and compareTo UC 1 method to test the maximum number
+
+//Result
+
+//Please Choose The Option Can Be Executed:
+//1.Find the Max Value Out Of Three:
+//2.Exit:
+//Enter the selected Option: 1
+//Enter the First Number:
+//23
+//Enter the Second Number:
+//32
+//Enter the Third Number:
+//8
+//32 is Maximum out of 23 ,32, 8
