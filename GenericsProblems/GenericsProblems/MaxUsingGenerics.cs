72e931e587141ebbce532bcabab817b22d69bbb9
4Shivaraj <4shivaraj.gowda@gmail.com>
2022-07-06 04:36:07 +0530

    [Shivaraj] Refactored Code To Generic Class

--- GenericsProblems/GenericsProblems/MaxUsingGenerics.cs
+++ GenericsProblems/GenericsProblems/MaxUsingGenerics.cs
@@ -6,7 +6,38 @@ using System.Threading.Tasks;
 
 namespace GenericsProblems
 {
+    public class FindMaxNum<T> where T : IComparable<T>
+    {
+        private T first_Value;
+        private T second_Value;
+        private T third_Value;
+        public FindMaxNum(T first_Value, T second_Value, T third_Value)
+        {
+            this.first_Value = first_Value;
+            this.second_Value = second_Value;
+            this.third_Value = third_Value;
+        }
+        public T GetMaximum()
+        {
+            if (first_Value.CompareTo(second_Value) > 0 && first_Value.CompareTo(third_Value) > 0)
+            {
+                return first_Value;
+            }
+            else if (second_Value.CompareTo(first_Value) > 0 && second_Value.CompareTo(third_Value) > 0)
+            {
+                return second_Value;
+            }
+            else if (third_Value.CompareTo(first_Value) > 0 && third_Value.CompareTo(second_Value) > 0)
+            {
+                return third_Value;
+            }
+            else
+            {
+                throw new Exception("Values are same");
+            }
 
+        }
+    }
     public class FindMaxNum
     {
         public int GetMaximumInteger(int first_Number, int second_Number, int third_Number)
@@ -87,6 +118,7 @@ namespace GenericsProblems
             }
         }
     }
+    
 }
 
 
